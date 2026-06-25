23bc64b7c373ba08e1b93d123a8273bc78eec08b
gnanasindhu2007-debug <gnanasindhu2007@gmail.com>
2026-06-25 17:46:41 +0530

    Fix DEBUG setting and restore settings comments

--- travellitineraryplanner/settings.py
+++ travellitineraryplanner/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5nyz9@w6pbd5=urvh631f52rj%4n4v)!x2z-eg3ee@5-(#&_am'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Flase
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
